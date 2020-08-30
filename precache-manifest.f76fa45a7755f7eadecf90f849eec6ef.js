0581663854366bdf31106c7d1e8d635f5ce3a162
inyong hwang <hiy911018@gmail.com>
2020-08-31 00:36:10 +0900

    Updates

---
+++ precache-manifest.f76fa45a7755f7eadecf90f849eec6ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97b38fdac9b55427e004eb2823ca456",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e746586d87976fa0d9f8",
+    "url": "/movie_app/static/css/main.621fc206.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e746586d87976fa0d9f8",
+    "url": "/movie_app/static/js/main.1e9ab7b6.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
